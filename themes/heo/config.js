a5550043ded64bb88286a805e089a0f4332139db
chenfy840 <128282445+chenfy840@users.noreply.github.com>
2023-08-10 07:28:35 +0800

    更新 config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -11,13 +11,13 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '分享编程',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
+  HERO_TITLE_3: '数码科技爱好者',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_5: '分享与热心帮助',
+  HERO_TITLE_LINK: 'https://haojun0129.link',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tags/必看精选' },
+  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
